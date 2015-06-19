b9c76b2c86b723e74909eed911415f1e9f8b332d
Adam Phelps <amp@opendns.com>
2015-06-18 23:08:05 -0700

    Initial FastLED conway's life

---
+++ ConwayLife/ConwayLife.ino
@@ -0,0 +1,124 @@
+#include <FastLED.h>
+
+#define DEBUG_LEVEL DEBUG_HIGH
+#include <Debug.h>
+
+#define LED_PIN  5
+
+#define COLOR_ORDER GRB
+#define CHIPSET     WS2812B
+
+#define BRIGHTNESS 255
+
+const uint8_t width = 20;
+const uint8_t height = 12;
+
+#define NUM_LEDS (width * height)
+CRGB leds_plus_safety_pixel[ NUM_LEDS + 1];
+CRGB* leds( leds_plus_safety_pixel + 1);
+
+#define CURRENT 0x1
+#define NEXT    0x2
+byte cells[width][height];
+
+void setup() {
+  Serial.begin(9600);
+
+  FastLED.addLeds<CHIPSET, LED_PIN, COLOR_ORDER>(leds, NUM_LEDS).setCorrection(TypicalSMD5050);
+  FastLED.setBrightness( BRIGHTNESS );
+
+  randomSeed(analogRead(0));
+
+  initialize(random(0,10));
+}
+
+
+uint16_t XY( uint8_t x, uint8_t y)
+{
+  return (y * width) + x;  
+}
+
+void initialize(uint32_t threshold) {
+  DEBUG1_VALUELN("Initing with threshold:", threshold);
+  for (int x = 0; x < width; x++) {
+    for (int y = 0; y < height; y++) {
+      if (random(0,10) < threshold) {
+        cells[x][y] = NEXT;
+      }
+    }
+  }
+}
+
+byte neighbors(byte x, byte y) {
+  byte count = 0;
+
+  if (x > 0) {
+    if (cells[x-1][y] & CURRENT) count++;
+    if ((y > 0) && (cells[x-1][y-1] & CURRENT)) count++;
+    if ((y < height - 1) && (cells[x-1][y+1] & CURRENT)) count++;
+  }
+
+  if ((y > 0) && (cells[x][y-1] & CURRENT)) count++;
+  if ((y < height - 1) && (cells[x][y+1] & CURRENT)) count++;
+
+  if (x < width - 1) {
+    if (cells[x+1][y] & CURRENT) count++;
+    if ((y > 0) && (cells[x+1][y-1] & CURRENT)) count++;
+    if ((y < height - 1) && (cells[x+1][y+1] & CURRENT)) count++;
+  }
+
+  return count;
+}
+
+void loop() {
+  uint32_t now = millis();
+
+  uint16_t changes = conways_rules();
+  DEBUG1_VALUELN("Changes:", changes);
+  if (changes == 0) {
+    delay(5000);
+    initialize(random(0,10));
+  }
+
+  for (int x = 0; x < width; x++) {
+    for (int y = 0; y < height; y++) {
+      if (cells[x][y] & NEXT) {
+        cells[x][y] = CURRENT;
+        leds[XY(x, y)] = CRGB(255, 0, 0);
+      } else {
+        cells[x][y] = 0;
+        leds[XY(x, y)] = CRGB(0, 0, 0);
+      }
+    }
+  }
+
+  FastLED.show();
+  delay(500);
+}
+
+uint16_t conways_rules() {
+  uint16_t changes = 0;
+
+  for (int x = 0; x < width; x++) {
+    for (int y = 0; y < height; y++) {
+      byte count = neighbors(x, y);
+
+      if (cells[x][y] & CURRENT) {
+        // Only lives on a 2 or 3
+        if (count == 2 || count == 3) {
+          cells[x][y] |= NEXT;
+        } else {
+          changes++;
+        }
+      } else {
+        // Reproduce on a 3
+        if (count == 3) {
+          cells[x][y] |= NEXT;
+          changes++;
+        }
+      }
+    }
+  }
+
+  return changes;
+}
