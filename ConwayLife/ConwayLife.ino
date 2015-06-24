90b6f5a823efb56681545cd61e6332675be1db0f
Adam Phelps <amp@opendns.com>
2015-06-24 01:32:32 -0700

    Added LCD display, refactored neighbors function

--- ConwayLife/ConwayLife.ino
+++ ConwayLife/ConwayLife.ino
@@ -1,6 +1,9 @@
 #include <FastLED.h>
 #include <math.h>
 
+#include "Wire.h"
+#include "LiquidCrystal.h"
+
 #define DEBUG_LEVEL DEBUG_HIGH
 #include <Debug.h>
 
@@ -22,6 +25,10 @@ CRGB* leds( leds_plus_safety_pixel + 1);
 #define NEXT    0x2
 byte cells[width][height];
 
+
+// Connect via i2c, default address #0 (A0-A2 not jumpered)
+LiquidCrystal lcd(0);
+
 void setup() {
   Serial.begin(9600);
 
@@ -29,6 +36,9 @@ void setup() {
   FastLED.setBrightness( BRIGHTNESS );
 
   randomSeed(analogRead(0));
+
+  lcd.begin(16,2);
+  lcd.setBacklight(HIGH);
 
   initialize(random(20,50));
 }
@@ -40,8 +50,20 @@ uint16_t XY( uint8_t x, uint8_t y)
 }
 
 uint32_t last_reset = 0;
+uint16_t age = 0;
+uint16_t max_age = 0;
 void initialize(uint32_t threshold) {
   DEBUG1_VALUELN("Initing with threshold:", threshold);
+
+  if (age > max_age)
+    max_age = age;
+
+  lcd.setCursor(0, 0);
+  lcd.print("Thre:");
+  lcd.print(threshold);
+  lcd.print(" Max:");
+  lcd.print(max_age);
+
   for (int x = 0; x < width; x++) {
     for (int y = 0; y < height; y++) {
       if (random(0,100) < threshold) {
@@ -54,23 +76,28 @@ void initialize(uint32_t threshold) {
   }
 
   FastLED.show();
+
   last_reset = millis();
+  age = 0;
 }
+
+#define CELL(x, y, ix, iy) \
+  (cells[(x + width + ix) % width][(y + height + iy) % height])
 
 byte neighbors(byte x, byte y) {
   byte count = 0;
 
-  if (cells[x-1 % width][y] & CURRENT) count++;
-  if ((cells[x-1 % width][y-1 % height] & CURRENT)) count++;
-  if ((cells[x-1 % width][y+1 % height] & CURRENT)) count++;
+  if (CELL(x, y, -1, -1) & CURRENT) count++;
+  if (CELL(x, y, -1,  0) & CURRENT) count++;
+  if (CELL(x, y, -1,  1) & CURRENT) count++;
 
-  if ((cells[x][y-1 % height] & CURRENT)) count++;
-  if ((cells[x][y+1 % height] & CURRENT)) count++;
+  if (CELL(x, y, 0, -1) & CURRENT) count++;
+  if (CELL(x, y, 0,  1) & CURRENT) count++;
 
-  if (cells[x+1 % width][y] & CURRENT) count++;
-  if ((cells[x+1 % width][y-1 % height] & CURRENT)) count++;
-  if ((cells[x+1 % width][y+1 % height] & CURRENT)) count++;
-  
+  if (CELL(x, y, 1, -1) & CURRENT) count++;
+  if (CELL(x, y, 1,  0) & CURRENT) count++;
+  if (CELL(x, y, 1,  1) & CURRENT) count++;
+
   return count;
 }
 
@@ -78,6 +105,8 @@ uint16_t prev_changes = 0;
 void loop() {
   uint16_t changes = conways_rules();
   uint16_t count = 0;
+
+  age++;
 
   DEBUG1_VALUE(" Changes:", changes);
 
@@ -125,6 +154,17 @@ void loop() {
 
   DEBUG1_VALUE(" Count:", count);
   DEBUG1_VALUELN(" Cycles:", cycles);
+
+  lcd.setCursor(0, 1);
+  lcd.print("");
+  lcd.print(changes);
+  lcd.print("-");
+  lcd.print(count);
+  lcd.print(" :");
+  lcd.print((int)cycles);
+  lcd.print("-");
+  lcd.print(age);
+  lcd.print("  ");
 
   static uint32_t repeating_start = 0;
   uint32_t elapsed = 0;
